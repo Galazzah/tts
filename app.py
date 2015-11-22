287599f11cde75f4c4646675c3f72ee337e4a256
timmyreilly <timreilly@live.com>
2015-11-22 00:05:22 -0800

    added index and home route

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+return render_template(
+	'index.html'
+	title = 'home'
+	wavfile = 'data:wav' + stream
+)
+
+if __name__ == "__main__":
+    app.run()
+	
+	
+
